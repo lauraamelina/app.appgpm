55e35732e9d39a1687c35555d0e69a3bfa41bf36
Laura Lopez <lauraamelina@gmail.com>
2023-10-10 12:42:22 -0300

    feat: filter services by Country

--- app/src/components/services/TableServices.jsx
+++ app/src/components/services/TableServices.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Swal from 'sweetalert2'
 
 
 export default function TableServices({ services, onSubmit }) {
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const [dataCountry, setDataCountry] = useState([])
+
   function getImage(image) {
     if (image) {
       return `https://api.appgpm.com/files/img/${image}`
     }
   }
+
+  useEffect(() => {
+    const countries = services.map((service) => service.enterprise.country.nombre)
+    const uniqueCountries = [...new Set(countries)]
+    setDataCountry(uniqueCountries)
+  }, [services])
+
 
   function handleView(service) {
     Swal.fire({
@@ -73,6 +83,22 @@ export default function TableServices({ services, onSubmit }) {
 
   return (
     <div className="tableDocuments p-1 table-responsive">
+      <div className="search-container mb-3">
+        <label htmlFor="countrySearch" className='form-label'>Buscar por país:</label>
+        <select
+          className="form-select"
+          id="countrySearch"
+          value={selectedCountry}
+          onChange={(e) => setSelectedCountry(e.target.value)}
+        >
+          <option value="">Selecciona un país</option>
+          {dataCountry.map((country, index) => (
+            <option key={index} value={country}>
+              {country}
+            </option>
+          ))}
+        </select>
+      </div>
       <table className="table">
         <thead>
           <tr>
@@ -85,21 +111,26 @@ export default function TableServices({ services, onSubmit }) {
           </tr>
         </thead>
         <tbody>
-          {services.map((service, index) => (
-            <tr key={index}>
-              <th scope="row">
-                <img className='imgService rounded' src={getImage(service?.enterprise?.avatar)} alt={service?.enterprise?.name} />
-              </th>
-              <td>{service?.nombre}</td>
-              <td>{service?.enterprise?.razon_social}</td>
-              <td>{service?.enterprise?.country?.nombre}</td>
-              <td>{service?.enterprise?.telefono}</td>
-              <td className='d-flex flex-column'>
-                <Link onClick={() => handleView(service)} className="btn btn-primary btn-sm mb-1">Ver descripción</Link>
-                <Link onClick={() => handleContact(service)} className="btn btn-secondary btn-sm">Contactar</Link>
-              </td>
-            </tr>
-          ))}
+          {services
+            .filter((service) =>
+              selectedCountry === '' ||
+              service.enterprise.country.nombre.toLowerCase().includes(selectedCountry.toLowerCase())
+            )
+            .map((service, index) => (
+              <tr key={index}>
+                <th scope="row">
+                  <img className='imgService rounded' src={getImage(service?.enterprise?.avatar)} alt={service?.enterprise?.name} />
+                </th>
+                <td>{service?.nombre}</td>
+                <td>{service?.enterprise?.razon_social}</td>
+                <td>{service?.enterprise?.country?.nombre}</td>
+                <td>{service?.enterprise?.telefono}</td>
+                <td className='d-flex flex-column'>
+                  <Link onClick={() => handleView(service)} className="btn btn-primary btn-sm mb-1">Ver descripción</Link>
+                  <Link onClick={() => handleContact(service)} className="btn btn-secondary btn-sm">Contactar</Link>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
